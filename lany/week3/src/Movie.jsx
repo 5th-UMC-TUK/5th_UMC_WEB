1ae77d1fdc8b0692b2de31d7ec32de5c288dd6c9
gs0428 <lider28@naver.com>
2023-10-04 02:50:18 +0900

    week3 mission

--- lany/week3/src/Movie.jsx
+++ lany/week3/src/Movie.jsx
@@ -1,10 +1,16 @@
 export default function Movie({ movieData }) {
   return (
     <div className="movie_wrap">
-      <img className="movie_img" src={movieData.backdrop_path} alt="none" />
-      <div className="movie_title_average">
-        <span>{movieData.title}</span>
-        <span>{movieData.vote_average}</span>
+      <div className="movie_detail_wrap">
+        <div className="movie_detail">
+          <h2>{movieData.title}</h2>
+          <div>{movieData.overview}</div>
+        </div>
+        <img className="movie_img" src={movieData.backdrop_path} alt="none" />
+        <div className="movie_title_average">
+          <span>{movieData.title}</span>
+          <span>{movieData.vote_average}</span>
+        </div>
       </div>
     </div>
   );
