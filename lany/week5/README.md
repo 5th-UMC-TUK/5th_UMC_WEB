8ac54bee480be82c6c652748e78c0b26469443f0
gs0428 <lider28@naver.com>
2023-10-15 11:51:10 +0900

    week4 미션

---
+++ lany/week5/README.md
@@ -0,0 +1,3 @@
+# 4주차 실습 체크리스트
+
+- styled-components 적용
---
+++ lany/week5/src/components/movie/Movie.style.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const MovieWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const MovieImg = styled.img`
+  width: 300px;
+  height: 450px;
+`;
+
+export const MovieTitleAverage = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: inherit;
+  padding: 20px;
+  color: rgb(255, 255, 255);
+  background-color: rgb(56, 59, 102);
+`;
+
+export const MovieDetailWrap = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MovieDetail = styled.div`
+  opacity: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  height: 100%;
+  color: rgb(255, 255, 255);
+  padding: 0px 10px;
+  transition: opacity 0.5s;
+  &:hover {
+    opacity: 1;
+  }
+`;
