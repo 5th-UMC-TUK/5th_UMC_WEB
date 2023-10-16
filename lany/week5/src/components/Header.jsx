de8d632b53da48e01ec022ee75ea078616a05761
gs0428 <lider28@naver.com>
2023-10-16 16:33:47 +0900

    chore : styled-components 추가

--- lany/week5/src/components/Header.jsx
+++ lany/week5/src/components/Header.jsx
@@ -1,64 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import * as H from '../style/header.style';
 
 export default function Header() {
   return (
-    <div
-      style={{
-        backgroundColor: 'rgb(35, 37, 72)',
-        padding: '10px 20%',
-        display: 'flex',
-        alignItems: 'center',
-      }}
-    >
-      <Link
-        to="/"
-        style={{
-          display: 'flex',
-          textDecoration: 'none',
-        }}
-      >
-        <div
-          style={{
-            position: 'relative',
-            fontSize: '20px',
-            top: '1px',
-            background: ' linear-gradient(to right, #2CC827 0%, #30FFD6 100%)',
-            webkitBackgroundClip: 'text',
-            webkitTextFillColor: 'transparent',
-            fontWeight: '700',
-            marginRight: '5px',
-          }}
-        >
-          TMBD
-        </div>
-        <div
-          style={{
-            width: '40px',
-            borderRadius: '10px',
-            background:
-              ' linear-gradient(180deg, rgba(48,255,214,1) 0%, rgba(27,208,221,1) 100%)',
-          }}
-        />
-      </Link>
-      <Link
-        to="/movie"
-        style={{ textDecoration: 'none', color: 'white', margin: '0px 20px' }}
-      >
-        영화
-      </Link>
-      <Link
-        to="/tv"
-        style={{ textDecoration: 'none', color: 'white', margin: '0px 20px' }}
-      >
-        TV프로그램
-      </Link>
-      <Link
-        to="/person"
-        style={{ textDecoration: 'none', color: 'white', margin: '0px 20px' }}
-      >
-        인물
-      </Link>
-    </div>
+    <H.Container>
+      <H.Logo to="/">
+        <H.LogoText>TMBD</H.LogoText>
+        <H.LogoRect />
+      </H.Logo>
+      <H.PageLink to="/movie">영화</H.PageLink>
+      <H.PageLink to="/tv">TV프로그램</H.PageLink>
+      <H.PageLink to="/person">인물</H.PageLink>
+    </H.Container>
   );
 }
