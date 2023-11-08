aa29b98cd988e020a24857f3f75888411318d747
gs0428 <lider28@naver.com>
2023-11-08 12:21:51 +0900

    week6 mission

--- lany/week6/src/components/home/header/LoginControl.jsx
+++ lany/week6/src/components/home/header/LoginControl.jsx
@@ -1,45 +1,18 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import * as L from '../../../style/home/header/logincontrol';
+import React, { useState } from "react";
+import * as L from "../../../style/home/header/logincontrol";
 
-class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isLoggedIn: false };
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-  }
+export default function LoginControl() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  handleLoginClick() {
-    this.setState(() => ({
-      isLoggedIn: true,
-    }));
-  }
+  const toggleBtn = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
 
-  handleLogoutClick() {
-    this.setState(() => ({
-      isLoggedIn: false,
-    }));
-  }
-
-  render() {
-    return (
-      <>
-        <L.LoginBtn
-          onClick={
-            this.state.isLoggedIn
-              ? this.handleLogoutClick
-              : this.handleLoginClick
-          }
-        >
-          {this.state.isLoggedIn ? '로그아웃' : '로그인'}
-        </L.LoginBtn>
-        <L.LoginStateText>
-          {this.state.isLoggedIn ? '환영합니다!' : '로그인 해주세요!'}
-        </L.LoginStateText>
-      </>
-    );
-  }
+  return (
+    <>
+      <L.LoginBtn onClick={toggleBtn}>{isLoggedIn ? "로그아웃" : "로그인"}</L.LoginBtn>
+      <L.LoginStateText>{isLoggedIn ? "환영합니다!" : "로그인 해주세요!"}</L.LoginStateText>
+    </>
+  );
 }
-
-export default LoginControl;
