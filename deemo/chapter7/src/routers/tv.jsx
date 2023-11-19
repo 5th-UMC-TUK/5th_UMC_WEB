7ffaa8f939dbb9a17b9adc93c853334e2d39fbc6
echo <play3step@naver.com>
2023-11-19 19:15:37 +0900

    7주차 미션

--- deemo/chapter7/src/routers/tv.jsx
+++ deemo/chapter7/src/routers/tv.jsx
@@ -1,7 +1,21 @@
-function TV(){
-    return (
-        <h1>TV프로그램</h1>
-    )
+import Movie from "../components/Movie/movie";
+import { tvDummy } from "../data/tvDummy";
+
+function TV() {
+  return (
+    <ul>
+      {tvDummy.results.map((movie) => (
+        <li key={movie.id}>
+          <Movie
+            img={movie.backdrop_path}
+            titleText={movie.name}
+            voteAverage={movie.vote_average}
+            movieId={movie.id}
+          />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default TV
+export default TV;
