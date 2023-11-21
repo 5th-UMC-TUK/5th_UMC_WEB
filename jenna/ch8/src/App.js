e38ece4139c42c866363dd569ae3b0363e5cac58
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2023-11-21 13:40:24 +0900

    ch8 실습체크리스트

---
+++ jenna/ch8/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+function App() {
+  const API_KEY = "9bbec6a0226a09bcae47d314828ef550";
+  const [city, setCity] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [weather, setWeather] = useState("");
+
+  const searchWeather = async () => {
+    try {
+      const result = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      );
+
+      console.log(result);
+      setTemperature(result.data.main.temp);
+      setWeather(result.data.weather[0].main);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (city !== "") {
+      searchWeather();
+    }
+  }, [city]);
+
+  const onActiveEnter = (e) => {
+    if (e.key === "Enter") {
+      setCity(e.target.value);
+    }
+  };
+
+  return (
+    <div>
+      <input placeholder="도시를 입력하세요" onKeyDown={onActiveEnter}></input>
+      <div>
+        {temperature !== "" && (
+          <>
+            <h3>{city}</h3>
+            <h1>{Math.round((temperature - 273.15) * 10) / 10}</h1>
+            <p>{weather}</p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
